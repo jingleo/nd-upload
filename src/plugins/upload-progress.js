71c0af8042a1d98f458a7d55140b3febc8328cf8
crossjs <liwenfu@crossjs.com>
2015-08-07 13:30:13 +0800

    update

--- src/plugins/upload-progress.js
+++ src/plugins/upload-progress.js
@@ -16,10 +16,10 @@ module.exports = function() {
   host.on('uploadProgress', function(file, percentage) {
     var progressbar = file.widget.$('[data-role="file-progress"]');
 
-    progressbar.css('width', (1-percentage) * 100 + '%');
+    progressbar.css('width', (1 - percentage) * 100 + '%');
 
     if (percentage === 1) {
-       progressbar.removeClass('active');
+      progressbar.removeClass('active').append('<span class="iconfont iconfont-ok"></span>');
     }
   });
 
